0ca61fb1105ec8db49e707648c5ee542b70444df
ThankedDeer <117323510+ThankedDeer@users.noreply.github.com>
2023-09-09 19:58:32 -0600

    Added challenge 1 sesion 1

---
+++ Reto1_S1/src/main/java/parseIntToBinary.java
@@ -0,0 +1,10 @@
+public class parseIntToBinary {
+    public static void main(String[] args) {
+        int numero = 40;
+
+        String binario = Integer.toBinaryString(numero);
+
+        System.out.println("El numero " + numero + " en binario es " + binario);
+
+    }
+}
